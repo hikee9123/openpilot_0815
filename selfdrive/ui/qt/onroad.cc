37f23b25d24a535db172cd5e1bd0f3e06c8f741f
hikee9123 <hikee9123@gmail.com>
2022-06-10 21:39:36 +0900

    dd

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -392,7 +392,7 @@ void NvgWindow::drawLaneLines(QPainter &painter, const UIState *s) {
     painter.setBrush(QColor::fromRgbF(1.0, 1.0, 1.0, std::clamp<float>(scene.lane_line_probs[i], 0.0, 0.7)));
     painter.drawPolygon(scene.lane_line_vertices[i].v, scene.lane_line_vertices[i].cnt);
   }
-*/
+
   // road edges
   for (int i = 0; i < std::size(scene.road_edge_vertices); ++i) {
     painter.setBrush(QColor::fromRgbF(1.0, 0, 0, std::clamp<float>(1.0 - scene.road_edge_stds[i], 0.0, 1.0)));
@@ -401,7 +401,7 @@ void NvgWindow::drawLaneLines(QPainter &painter, const UIState *s) {
       painter.drawPolygon(scene.road_edge_vertices[i].v, scene.road_edge_vertices[i].cnt);
   }
 
-/*
+
   // paint path
   QLinearGradient bg(0, height(), 0, height() / 4);
   if (scene.end_to_end) {
@@ -423,9 +423,7 @@ void NvgWindow::drawLaneLines(QPainter &painter, const UIState *s) {
     bg.setColorAt(1, whiteColor(0));
   }
   painter.setBrush(bg);
-  if( scene.track_vertices.cnt > 0 )
-    painter.drawPolygon(scene.track_vertices.v, scene.track_vertices.cnt);
-
+  painter.drawPolygon(scene.track_vertices.v, scene.track_vertices.cnt);
 */
 
 
