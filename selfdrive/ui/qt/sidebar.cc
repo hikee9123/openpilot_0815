a07ee4bb5fc3befe39926319647b519a6ad4a86d
hikee9123 <hikee9123@gmail.com>
2022-06-25 19:44:17 +0900

    fix

--- selfdrive/ui/qt/sidebar.cc
+++ selfdrive/ui/qt/sidebar.cc
@@ -132,9 +132,9 @@ void Sidebar::paintEvent(QPaintEvent *event) {
   p.drawText(r, Qt::AlignCenter, net_type);
 
   // metrics
-  drawMetric(p, temp_status.first, temp_status.second, 340);
-  drawMetric(p, panda_status.first, panda_status.second, 518);
-  drawMetric(p, connect_status.first, connect_status.second, 676);
+  drawMetric(p, temp_status.first, temp_status.second, 378);
+  drawMetric(p, panda_status.first, panda_status.second, 558);
+  drawMetric(p, connect_status.first, connect_status.second, 716);
 
 
 
