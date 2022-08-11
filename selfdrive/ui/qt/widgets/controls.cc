81e288f99fa184044d9badb74df84d09cb97c85c
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2022-08-11 20:02:33 +0900

    Update controls.cc

--- selfdrive/ui/qt/widgets/controls.cc
+++ selfdrive/ui/qt/widgets/controls.cc
@@ -153,8 +153,8 @@ MenuControl::MenuControl( const QString &str_param, const QString &title, const 
   label.setAlignment(Qt::AlignVCenter|Qt::AlignRight);
   label.setStyleSheet( R"(
     color: #e0e879;
-    font-size: 30px;
-    font-weight: 100;    
+    font-size: 70px;
+    font-weight: 300;    
     background-color: transparent;    
   )");
 
