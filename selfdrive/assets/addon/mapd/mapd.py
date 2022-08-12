bb69294260c7120d43a52fe6efc50aefae4c30d6
hikee9123 <hikee9123@gmail.com>
2022-08-12 23:35:23 +0900

    fix

--- selfdrive/assets/addon/mapd/mapd.py
+++ selfdrive/assets/addon/mapd/mapd.py
@@ -59,7 +59,11 @@ class MapD():
 
   def update_param(self):
     self._PRE_LANE_DISTANCE = int( Params().get("OpkrOSM_PRE_LANE_DISTANCE") ) # 100.  #  1 about 1M distance
-   
+    QUERY_RADIUS = int( Params().get("OpkrOSM_QUERY_RADIUS", encoding="utf8") )   # def: 3000  # mts. Radius to use on OSM data queries.
+    MIN_DISTANCE_FOR_NEW_QUERY = int( Params().get("OpkrOSM_MIN_DISTANCE_FOR_NEW_QUERY", encoding="utf8") ) # 1000  # mts. Minimum distance to query area edge before issuing a new query.
+    FULL_STOP_MAX_SPEED = float( Params().get("OpkrOSM_FULL_STOP_MAX_SPEED", encoding="utf8") ) # 1.39  # m/s Max speed for considering car is stopped.
+    LOOK_AHEAD_HORIZON_TIME =  float( Params().get("OpkrOSM_LOOK_AHEAD_HORIZON_TIME", encoding="utf8") )  #15.  # s. Time horizon for look ahead of turn speed sections to provide on liveMapData msg.
+      
 
   def udpate_state(self, sm):
     sock = 'controlsState'
