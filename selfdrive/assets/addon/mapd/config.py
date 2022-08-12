e6b10ba714ad69fc6f7d60ff9068387ab38fbc89
hikee9123 <hikee9123@gmail.com>
2022-08-12 23:18:48 +0900

    fix

--- selfdrive/assets/addon/mapd/config.py
+++ selfdrive/assets/addon/mapd/config.py
@@ -9,4 +9,4 @@ MIN_DISTANCE_FOR_NEW_QUERY = int( Params().get("OpkrOSM_MIN_DISTANCE_FOR_NEW_QUE
 FULL_STOP_MAX_SPEED = float( Params().get("OpkrOSM_FULL_STOP_MAX_SPEED", encoding="utf8") ) # 1.39  # m/s Max speed for considering car is stopped.
 LOOK_AHEAD_HORIZON_TIME =  float( Params().get("OpkrOSM_LOOK_AHEAD_HORIZON_TIME", encoding="utf8") )  #15.  # s. Time horizon for look ahead of turn speed sections to provide on liveMapData msg.
 LANE_WIDTH = float( Params().get("OpkrOSM_LANE_WIDTH", encoding="utf8") )   #3.7  # Lane width estimate. Used for detecting departures from way.
-PRE_LANE_DISTANCE = float( Params().get("OpkrOSM_PRE_LANE_DISTANCE", encoding="utf8") ) # 100.  #  1 about 1M distance
+PRE_LANE_DISTANCE = int( Params().get("OpkrOSM_PRE_LANE_DISTANCE", encoding="utf8") ) # 100.  #  1 about 1M distance
