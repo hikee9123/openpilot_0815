7ee53b648579095fb2b251b9e5b8c62cb4b2d124
hikee9123 <hikee9123@gmail.com>
2022-08-16 17:38:32 +0900

    test

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -231,10 +231,10 @@ void NvgWindow::updateState(const UIState &s) {
 void NvgWindow::drawHud(QPainter &p) {
   p.save();
 
-  float speedLimit = 100;
+  float speedLimit = 0;
   float setSpeed = 110; 
-  bool has_us_speed_limit = true;
-  bool has_eu_speed_limit = false;
+  bool has_us_speed_limit = false;
+  bool has_eu_speed_limit = true;
   float speed = 60;
   bool is_metric = false;
   bool rightHandDM = false;
