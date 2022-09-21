c8dfec95291b1b32a5e7dc05f8c39a97c89b219d
hikee9123 <hikee9123@gmail.com>
2022-09-22 00:07:30 +0900

    fix

--- selfdrive/locationd/torqued.py
+++ selfdrive/locationd/torqued.py
@@ -247,7 +247,8 @@ class TorqueEstimator:
 
 
 def main(sm=None, pm=None):
-  config_realtime_process([0, 1, 2, 3], 5)
+  #config_realtime_process([0, 1, 2, 3], 5)
+  config_realtime_process(3, 55)
 
   if sm is None:
     sm = messaging.SubMaster(['carControl', 'carState', 'liveLocationKalman'], poll=['liveLocationKalman'])
