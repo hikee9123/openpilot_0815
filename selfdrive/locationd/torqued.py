b934894b258c82abdd05f3ae5dc732e03852a6ed
hikee9123 <hikee9123@gmail.com>
2022-09-24 18:17:49 +0900

    fix

--- selfdrive/locationd/torqued.py
+++ selfdrive/locationd/torqued.py
@@ -243,7 +243,7 @@ class TorqueEstimator:
     liveTorqueParameters.totalBucketPoints = len(self.filtered_points)
     liveTorqueParameters.decay = self.decay
     liveTorqueParameters.maxResets = self.resets
-    print( 'live torq {}'.format(liveTorqueParameters) )
+    print( 'live torq {}'.format( msg ) )
     return msg
 
 
