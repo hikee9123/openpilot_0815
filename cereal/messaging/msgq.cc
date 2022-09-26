866b8acebc94722b41b3f965f3f5983ced9c445c
hikee9123 <hikee9123@gmail.com>
2022-09-26 20:14:03 +0900

    fix

--- cereal/messaging/msgq.cc
+++ cereal/messaging/msgq.cc
@@ -336,7 +336,7 @@ int msgq_msg_recv(msgq_msg_t * msg, msgq_queue_t * q){
   assert(id >= 0); // Make sure subscriber is initialized
 
   if (q->read_uid_local != *q->read_uids[id]){
-    std::cout << q->endpoint << ": Reader was evicted, 2.reconnecting" << std::endl;
+    //std::cout << q->endpoint << ": Reader was evicted, 2.reconnecting" << std::endl;
     msgq_init_subscriber(q);
     goto start;
   }
