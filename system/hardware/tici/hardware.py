d1a43c37a31b0f53fb850ce2ee4c7fc92e55f63f
hikee9123 <hikee9123@gmail.com>
2022-10-06 20:17:01 +0900

    fix : tici configure_modem

--- system/hardware/tici/hardware.py
+++ system/hardware/tici/hardware.py
@@ -466,17 +466,17 @@ class Tici(HardwareBase):
     sim_id = self.get_sim_info().get('sim_id', '')
 
     # configure modem as data-centric
-      cmds = [
+    cmds = [
         'AT+QNVW=5280,0,"0102000000000000"',
         'AT+QNVFW="/nv/item_files/ims/IMS_enable",00',
         'AT+QNVFW="/nv/item_files/modem/mmode/ue_usage_setting",01',
-      ]
-      modem = self.get_modem()
-      for cmd in cmds:
-        try:
-          modem.Command(cmd, math.ceil(TIMEOUT), dbus_interface=MM_MODEM, timeout=TIMEOUT)
-        except Exception:
-          pass
+    ]
+    modem = self.get_modem()
+    for cmd in cmds:
+      try:
+        modem.Command(cmd, math.ceil(TIMEOUT), dbus_interface=MM_MODEM, timeout=TIMEOUT)
+      except Exception:
+        pass
 
     # blue prime config
     if sim_id.startswith('8901410'):
